87fdd87d0f73b90085cd0dd1e7e9ed4f033da397
kingle1024 <teran1024@naver.com>
2023-01-06 15:31:18 +0900

    fix : mapper 추가로 의존성 추가

--- src/main/java/com/dz/shop/Dao/AdminProductDAO.java
+++ src/main/java/com/dz/shop/Dao/AdminProductDAO.java
@@ -2,9 +2,11 @@ package com.dz.shop.Dao;
 
 import com.dz.shop.Page.BoardParam;
 import com.dz.shop.entity.ProductVO;
+import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
 
+@Mapper
 public interface AdminProductDAO {
     List<ProductVO> list(BoardParam boardParam);
     long listSize(String search);
--- src/main/java/com/dz/shop/Dao/CommentDAO.java
+++ src/main/java/com/dz/shop/Dao/CommentDAO.java
@@ -2,8 +2,10 @@ package com.dz.shop.Dao;
 
 import com.dz.shop.Page.BoardParam;
 import com.dz.shop.entity.CommentVO;
+import org.apache.ibatis.annotations.Mapper;
 import java.util.List;
 
+@Mapper
 public interface CommentDAO {
     List<CommentVO> list(BoardParam boardParam);
     long listSize(String search);
