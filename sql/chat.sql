db5910a1c2996898a6e851eb6910174e3244dbf8
kingle1024 <teran1024@naver.com>
2023-01-17 09:05:00 +0900

    : sql 파일 업로드

---
+++ sql/chat.sql
@@ -0,0 +1,5 @@
+insert into kosa.chat (no, chat_room, used_date)
+values  (70, '7459dd4a2a7b49589007a8442c771127', null),
+        (71, '98eb8775d5f44c3997f0318d14e63002', null),
+        (72, 'e1d754be856040168f69643dacc90bea', null),
+        (73, '1d37152a7c7e4e2a840e3ffaa2e73f51', null);
---
+++ sql/delivery_status.sql
@@ -0,0 +1,4 @@
+insert into kosa.delivery_status (status, no, name)
+values  ('READY', 100, '배송준비'),
+        ('FINISH', 300, '배송완료'),
+        ('SENDING', 200, '배송중');
