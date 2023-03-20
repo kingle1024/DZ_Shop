af8e342cf159dc065a458d0622206b58d1b6086e
kingle1024 <teran1024@naver.com>
2023-03-20 22:14:14 +0900

    feat : 메인 페이지 페이징 추가

--- src/main/java/com/dz/shop/HomeController.java
+++ src/main/java/com/dz/shop/HomeController.java
@@ -3,9 +3,8 @@ package com.dz.shop;
 import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
-
-import com.dz.shop.mappers.MemberDAO;
-import com.dz.shop.service.MemberService;
+import com.dz.shop.Page.PageUtil;
+import com.dz.shop.service.ProductService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,24 +12,33 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class HomeController {		
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 
 	@Autowired
-	MemberDAO memberDAO;
+	ProductService productService;
+
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
+	public String main(HttpServletRequest request, Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
-		
+
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
+
 		String formattedDate = dateFormat.format(date);
 		model.addAttribute("serverTime", formattedDate );
 
-		return "home";
+		String search = request.getParameter("search");
+		String pageIndex = request.getParameter("pageIndex");
+
+		PageUtil pageUtil = productService.pageUtil(search, pageIndex, "product");
+
+		model.addAttribute("list", pageUtil.getList());
+		model.addAttribute("pager", pageUtil.paper());
+
+		return "main";
 	}
-	
 }
