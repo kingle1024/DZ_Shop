9ca3a0de070e8b1fda30ce8fef3eccae588bbd1d
kingle1024 <teran1024@naver.com>
2023-03-23 22:07:24 +0900

    feat : 로그인 인터셉터 추가

--- src/main/java/com/dz/shop/Interceptor/LoginInterceptor.java
+++ src/main/java/com/dz/shop/Interceptor/LoginInterceptor.java
@@ -1,8 +1,8 @@
 package com.dz.shop.Interceptor;
 
+import com.dz.shop.Utility.SessionAttribute;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -10,17 +10,19 @@ import javax.servlet.http.HttpSession;
 public class LoginInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        System.out.println("LoginInterceptor.preHandle");
+        HttpSession session = request.getSession();
         response.setCharacterEncoding("UTF-8");
         response.setContentType("text/html;charset=UTF-8");
+        Object userId = session.getAttribute(SessionAttribute.userid.toString());
+        Object isAdmin = session.getAttribute(SessionAttribute.admin.toString());
 
-        HttpSession session = request.getSession();
-        String id = (String) session.getAttribute("sessionUserId");
-        if(id == null){
+        if(userId == null){
             response.sendRedirect(request.getContextPath()+"/member/loginForm.do");
             return false;
+        }else if(isAdmin == null){
+            response.sendRedirect(request.getContextPath()+"/");
+            return false;
         }
-
 
         return true;
     }
